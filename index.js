2de6db4b50b4b7d596670f3721b1436272d4ecf6
vxllx209 <bvillagrancalderon@gmail.com>
2026-09-01 11:26:18 -0400

    Actualización sitio

--- index.js
+++ index.js
@@ -2,16 +2,32 @@ const express = require('express');
 
 const app = express();
 
+const PORT = 3000;
+
+// Configurar ejs
+app.set('view engine', 'ejs');
+
+// Rutas
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World</h1>');
+    res.render('index');
+});
+
+// Rutas
+app.get('/nosotros', (req, res) => {
+    res.render('nosotros');
 });
 
 // Rutas
 app.get('/servicios', (req, res) => {
-    res.send('<h1>Servicios</h1>');
+    res.render('servicios');
+});
+
+// Rutas
+app.get('/contacto', (req, res) => {
+    res.render('contacto'); 
 });
 
 // Levantar Servidor
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
