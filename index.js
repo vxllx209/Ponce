2bcbb9c6323378b87565f3daf31bebfd6e04ba90
vxllx209 <bvillagrancalderon@gmail.com>
2026-09-01 11:01:42 -0400

    Nodemon

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send('<h1>Hello World</h1>');
 });
 
+// Rutas
+app.get('/servicios', (req, res) => {
+    res.send('<h1>Servicios</h1>');
+});
+
+// Levantar Servidor
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
 });
