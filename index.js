547a80d8972f5df3db9e5057d70dd9ac3d78a87c
vxllx209 <bvillagrancalderon@gmail.com>
2026-09-02 09:34:25 -0400

    Jujutsu

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
+require('dotenv').config()
+
 const express = require('express');
+
+const path = require('path');
 
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Configurar ejs
 app.set('view engine', 'ejs');
+
+// Configurar estáticos
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
 
 // Rutas
 app.get('/', (req, res) => {
